78735fc33968065b1f63156e01b2acb5a4e9bc3f
Marius Meschter <git@onestay.moe>
2022-02-10 14:46:46 +0100

    remove this validator

--- sc_tracker/validator.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-
-def validate_nonzero_positive(value):
-    if value <= 0:
-        raise ValidationError(
-            _("%(value) can't be smaller or equal to zero"), params={"value": value}
-        )
